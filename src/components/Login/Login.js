cf52a4ba7f179def71ce5f22b1224f38270b4fc4
ahsan <makabir98@yahoo.com>
2022-05-01 11:56:19 +0600

    google sign in method implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{margin : '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign in</button>
+            </div>
+            <form>
+                <input type="email" placeholder='Your email' />
+                <br />
+                <input type="password" placeholder='your password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
